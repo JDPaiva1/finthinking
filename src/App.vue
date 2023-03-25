65db49cbb9f7e4248be7da32780c4fa1a2552e09
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2023-03-25 18:49:54 +0000

    init project

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  @apply leading-[1.5] max-h-screen;
+}
+
+.logo {
+  @apply block mx-auto mb-8;
+}
+
+nav {
+  @apply w-full text-[12px] text-center mt-8;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  @apply bg-transparent;
+}
+
+nav a {
+  @apply inline-block py-0 px-4 border-l border-solid;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  @apply border-0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    @apply flex place-items-center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    @apply m-0 mr-8;
+  }
+
+  header .wrapper {
+    @apply flex place-items-start flex-wrap;
+  }
+
+  nav {
+    @apply text-left -ml-4 text-base pt-4 mt-4;
+  }
+}
+</style>
---
+++ src/components/WelcomeItem.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="item">
+    <i>
+      <slot name="icon"></slot>
+    </i>
+    <div class="details">
+      <h3>
+        <slot name="heading"></slot>
+      </h3>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.item {
+  @apply mt-8 flex;
+}
+
+.details {
+  @apply flex-[1] ml-4;
+}
+
+i {
+  @apply flex place-items-center place-content-center w-8 h-8;
+  color: var(--color-text);
+}
+
+h3 {
+  @apply text-lg font-medium mb-2;
+  color: var(--color-heading);
+}
+
+@media (min-width: 1024px) {
+  .item {
+    @apply mt-0 pt-[0.4rem] pr-0 pb-4;
+    padding-left: calc(var(--section-gap) / 2);
+  }
+
+  i {
+    @apply -left-6 absolute border border-solid border-r-8 w-1/2 h-1/2;
+    top: calc(50% - 25px);
+    border: 1px solid var(--color-border);
+    background: var(--color-background);
+  }
+
+  .item:before {
+    @apply content-none border-l border-solid absolute left-0;
+    border-left: 1px solid var(--color-border);
+    bottom: calc(50% + 25px);
+    height: calc(50% + 25px);
+  }
+
+  .item:after {
+    @apply content-none border-l border-solid absolute left-0;
+    border-left: 1px solid var(--color-border);
+    top: calc(50% - 25px);
+    height: calc(50% - 25px);
+  }
+
+  .item:first-of-type:before {
+    @apply hidden;
+  }
+
+  .item:last-of-type:after {
+    @apply hidden;
+  }
+}
+</style>
