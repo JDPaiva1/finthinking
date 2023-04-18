ace98eb60410b3e71104c753761a48f7d39e0298
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2023-04-18 15:47:37 +0100

    Fix type

--- src/helpers/utils.helper.ts
+++ src/helpers/utils.helper.ts
@@ -3,7 +3,7 @@ import type { Transactions } from '@/interfaces/types';
 export function orderByDate(transactions: Transactions) {
   const sorted = Object.entries(transactions)
     .reverse()
-    .sort((a, b) => new Date(b[1].date).getTime() - new Date(a[1].date).getTime());
+    .sort((a:any, b:any) => new Date(b[1].date).getTime() - new Date(a[1].date).getTime());
 
   const obj: Transactions = {};
 
