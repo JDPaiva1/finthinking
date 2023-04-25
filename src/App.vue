814e0e06e8961ee454bc4b972e577fdd5f89f9c1
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2023-04-25 20:43:13 +0100

    Add go back menu

--- src/App.vue
+++ src/App.vue
@@ -16,15 +16,21 @@ function signOutBtn() {
 </script>
 
 <template>
+  <header class="h-16 p-4 w-full relative flex justify-center items-center" v-if="route.name !== 'login' && route.name !== 'signup' && route.name !== 'forgot' && route.name !== 'home'">
+    <RouterLink to="/" class="absolute left-2 top-1/2 -translate-y-1/2 dark:text-indigo-400 text-indigo-600">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+      </svg>
+    </RouterLink>
+    <h3 class="mx-auto capitalize">{{ route.name }}</h3>
+  </header>
   <RouterView />
 
-  <header class="wrapper" v-if="route.name !== 'login' && route.name !== 'signup' && route.name !== 'forgot'">
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/new">New</RouterLink>
-      <a @click="signOutBtn">Sign-Out</a>
-    </nav>
-  </header>
+  <nav class="wrapper" v-if="route.name !== 'login' && route.name !== 'signup' && route.name !== 'forgot'">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/new">New</RouterLink>
+    <a @click="signOutBtn">Sign-Out</a>
+  </nav>
 </template>
 
 <style scoped>
