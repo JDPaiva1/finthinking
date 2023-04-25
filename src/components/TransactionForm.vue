4fe86048489123e9e1d6ece7e702febe02ce6e8f
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2023-04-25 20:25:09 +0100

    Add switch in transaction form

--- src/components/TransactionForm.vue
+++ src/components/TransactionForm.vue
@@ -5,50 +5,34 @@ import { useStore } from '@/stores/store';
 import type { Transaction } from '@/interfaces/types';
 import InputComponent from '@/components/atoms/InputComponent.vue';
 
-const title = ref<string>();
-const amount = ref<number>();
-const category = ref<string>();
-const date = ref<string>();
-const transaction = ref<Transaction>();
-
-const todayDate = ref(new Date().toJSON().split('T')[0]);
-
-const store = useStore();
-const router = useRouter();
-const route = useRoute();
-
-const isEditForm = !!route.params?.id;
+const todayDate = new Date().toJSON().split('T')[0],
+  txnForm = ref<Transaction>({ title: '', amount: 0, category: '', date: todayDate }),
+  isIncome = ref(false),
+  store = useStore(),
+  router = useRouter(),
+  route = useRoute(),
+  isEditForm = !!route.params?.id;
 
 if(isEditForm) {
-  store.getTransaction(route.params.id).then((data:Transaction) => {
-    transaction.value = data;
-    title.value = data.title;
-    amount.value = data.amount;
-    category.value = data.category;
-    date.value = data.date;
+  store.getTransaction(route.params.id).then((data: Transaction) => {
+    txnForm.value = data;
+    isIncome.value = txnForm.value.amount > 0;
   }).catch(error => {
     error;
   });
 }
 
-function getFormData():Transaction {
-  return {
-    title: title.value || '',
-    amount: amount.value || 0,
-    category: category.value || '',
-    date: date.value || todayDate.value
-  };
-}
-
 function saveTransaction() {
-  if(!(title.value || amount.value || category.value || date.value)) {
+  if (!(txnForm.value.title || txnForm.value.amount || txnForm.value.category || txnForm.value.date)) {
     return;
   }
 
+  updateAmountSign();
+
   if(isEditForm) {
-    store.editTransaction(route.params.id, getFormData());
+    store.editTransaction(route.params.id, txnForm.value);
   } else {
-    store.addTransaction(getFormData());
+    store.addTransaction(txnForm.value);
   }
 
   router.push({ name: 'home' });
@@ -58,14 +42,28 @@ function deleteTransaction() {
   store.deleteTransaction(route.params.id);
   router.push({ name: 'home' });
 }
+
+function toggle() {
+  isIncome.value = !isIncome.value;
+  updateAmountSign();
+}
+
+function updateAmountSign() {
+  txnForm.value.amount = isIncome.value ? Math.abs(txnForm.value.amount) : -Math.abs(txnForm.value.amount);
+}
 </script>
 
 <template>
+  <label for="switch" class="t-form-switch">
+    <input name="switch" type="checkbox" class="t-form-switch-input" v-model="isIncome">
+    <span class="t-form-switch-span rounded-l-md bg-gray-300" :class="{ 'bg-red-600': !isIncome }" @click="toggle">Expense</span>
+    <span class="t-form-switch-span rounded-r-md bg-gray-300" :class="{ 'bg-green-600': isIncome }" @click="toggle">Income</span>
+  </label>
   <div class="t-form">
-    <InputComponent label="Title" type="text" name="title" required v-model:value="title" />
-    <InputComponent label="Amount" type="number" name="amount" required v-model:value="amount" />
-    <InputComponent label="Category" type="select" name="category" :options="store.categories" v-model:value="category" />
-    <InputComponent label="Date" type="date" name="date" :max="todayDate" required v-model:value="date" />
+    <InputComponent label="Title" type="text" name="title" required v-model:value="txnForm.title" />
+    <InputComponent label="Amount" type="number" name="amount" required v-model:value="txnForm.amount" />
+    <InputComponent label="Category" type="select" name="category" :options="store.categories" v-model:value="txnForm.category" />
+    <InputComponent label="Date" type="date" name="date" :max="todayDate" required v-model:value="txnForm.date" />
     <button class="form-btn" @click="saveTransaction">
       {{ isEditForm ? 'Update Transaction' : 'Add Transaction' }}
     </button>
@@ -78,5 +76,15 @@ function deleteTransaction() {
 <style scoped>
 .t-form {
   @apply flex min-h-full flex-1 flex-col justify-center px-2 py-8;
+}
+
+.t-form-switch {
+  @apply flex w-full items-center p-2 rounded-md cursor-pointer text-black;
+}
+.t-form-switch-input {
+  @apply hidden;
+}
+.t-form-switch-span {
+  @apply w-full px-4 py-2;
 }
 </style>
