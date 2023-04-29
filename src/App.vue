db60e3e01de752f70ebf5a24f9fe0b03608e9dde
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2023-04-29 18:24:49 +0100

    Create font icon

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,10 @@ const route = useRoute();
 <template>
   <header class="navbar" v-if="route.name !== 'login' && route.name !== 'signup' && route.name !== 'forgot'">
     <RouterLink to="/" class="navbar-link" v-if="route.name !== 'home'">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-      </svg>
+      <i class="icon-arrow-left"></i>
     </RouterLink>
     <RouterLink to="/profile" class="navbar-link" v-else>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
-      </svg>
+      <i class="icon-user-circle"></i>
     </RouterLink>
     <h3 class="navbar-title">{{ route.name }}</h3>
   </header>
@@ -22,9 +18,7 @@ const route = useRoute();
   <RouterView />
 
   <RouterLink to="/new" class="add-button" v-if="route.name == 'home'">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-12 w-12">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-    </svg>
+    <i class="icon-plus"></i>
   </RouterLink>
 </template>
 
@@ -40,5 +34,8 @@ const route = useRoute();
 }
 .add-button {
   @apply fixed bottom-8 right-8 h-14 w-14 rounded-full bg-indigo-600 text-white flex justify-center content-center items-center;
+}
+.add-button .icon-plus {
+  @apply text-4xl;
 }
 </style>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -17,10 +17,8 @@ function signOutBtn() {
 
 <template>
   <main class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-    <div class="mx-auto w-full max-w-sm">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-16 h-16 mx-auto">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-      </svg>
+    <div class="mx-auto w-full max-w-sm text-center">
+      <i class="icon-user-circle text-9xl"></i>
       <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight">
         {{ store.fullname }}
       </h2>
