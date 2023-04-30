48bad3e98b98f8ab2a559ee85b54d2cd55514165
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2023-04-30 19:20:28 +0100

    Create Graphic component

--- src/views/GraphicView.vue
+++ src/views/GraphicView.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
+import GraphicComponent from '@/components/GraphicComponent.vue';
+import TransactionsList from '@/components/TransactionsList.vue';
 
+import { useStore } from '@/stores/store';
+
+const store = useStore();
 </script>
 
 <template>
   <main class="">
-    
+    <GraphicComponent />
+    <TransactionsList :transactions="store.transactions"/>
   </main>
 </template>
 
