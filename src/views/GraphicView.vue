443e42a78bb21dde3b98707777162c930dce808a
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2023-05-04 20:42:09 +0100

    Create this month chart

--- src/views/GraphicView.vue
+++ src/views/GraphicView.vue
@@ -5,12 +5,14 @@ import TransactionsList from '@/components/TransactionsList.vue';
 import { useStore } from '@/stores/store';
 
 const store = useStore();
+
+const txn = store.getTransactionsByMonth(new Date().getMonth());
 </script>
 
 <template>
   <main class="">
-    <GraphicComponent />
-    <TransactionsList :transactions="store.transactions"/>
+    <GraphicComponent :transactions="txn"/>
+    <TransactionsList :transactions="txn" title="thisMonth"/>
   </main>
 </template>
 
